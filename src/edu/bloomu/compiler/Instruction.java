8b1aa42290ee60458c7bc9af1eed191137607843
Max Norfolk <mnorfolk03@outlook.com>
2021-04-05 14:29:32 -0400

    implemented environment and instruction

--- src/edu/bloomu/compiler/Instruction.java
+++ src/edu/bloomu/compiler/Instruction.java
@@ -1,9 +1,26 @@
 package edu.bloomu.compiler;
 
+
+import edu.bloomu.compiler.value.Value;
+import edu.bloomu.compiler.value.function.Function;
+
 /**
  * Represents a single instruction
  *
  * @author Maxwell Norfolk
  */
 public class Instruction {
+
+    private Function func;
+    private Environment host;
+    private String[] params;
+
+    public void call() {
+        Value[] vParams = new Value[params.length];
+        for (int i = 0; i < params.length; i++) {
+            vParams[i] = host.find(params[i]);
+        }
+
+        func.callOn(vParams);
+    }
 }
