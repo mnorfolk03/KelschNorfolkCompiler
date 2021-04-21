08a50ac42b1c97d57a966c5190597f68f5630baf
skelsch <sk45893@huskies.bloomu.edu>
2021-04-21 11:06:15 -0400

    Added documentation and changed variable names for readability. Deleted second Compiler.java

--- src/edu/bloomu/compiler/Compiler.java
+++ src/edu/bloomu/compiler/Compiler.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 /**
  * Prompts the user with a file navigator window. The user should find a text file
  * containing code for a program in this language. The program in this file will be
- * tokenized.
+ * tokenized and executed.
  *
  * @author Schyler Kelsch
  * @author Maxwell Norfolk
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 public class Compiler {
     public static void main(String[] args) {
         // display file chooser and get selected file
-        JFileChooser chooser = new JFileChooser("F:IntelliJProjects/KelschNorfolkCompiler/resources");
+        JFileChooser chooser = new JFileChooser(new File(System.getProperty("user.dir")));
         FileNameExtensionFilter filter = new FileNameExtensionFilter(".txt files", "txt");
         chooser.setFileFilter(filter);
         File file;
@@ -33,36 +33,40 @@ public class Compiler {
         }
         file = chooser.getSelectedFile();
 
-        // scan input file
+        // scan input file and run program
         try {
             ArrayList<String[]> program = new ArrayList<>();
             Scanner input = new Scanner(file);
 
+            // read each line of the file
             while (input.hasNextLine()) {
                 ArrayList<String> tokens = new ArrayList<>();
                 String instruction = input.nextLine();
-                if (instruction.isEmpty() || instruction.startsWith("//"))
+                
+                // blank line or comment... no need to go further
+                if (instruction.isEmpty() || instruction.startsWith("//")) {
                     continue;
-
-                Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(instruction);
-                while (m.find()) {
-                    String s = m.group(1);
-                    if (!s.equals(" "))
-                        tokens.add(s);
                 }
-                String[] test = tokens.toArray(new String[0]);
-                program.add(test);
+
+                // tokenize line
+                Matcher line = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(instruction);
+                while (line.find()) {
+                    String token = line.group(1);
+                    if (!token.equals(" ")) {
+                        tokens.add(token);
+                    }
+                }
+                program.add(tokens.toArray(new String[0]));
             }
 
+            // set up execution environment
             program.add(0, new String[0]); // no params
             Environment builtin = new Environment(null);
-
             HostEnvironmentSetup.setup(builtin);
 
+            // execute program
             UserDefinedFunction main = UserDefinedFunction.parse(program);
             main.callOn(builtin);
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
